252ebb8c758d0328e3ab20b034e770798b43ab99
theotherdy <damion@allotmentor.com>
2018-06-22 11:28:49 +0100

    ou menu-demo

--- allmethods/allmethods_web.js
+++ allmethods/allmethods_web.js
@@ -1,46 +1,110 @@
-/** 
- * This JavaScript file contains js to create a rh menu in Instructure Canvas
- * 
- * @projectname OU Canvas Menu
- * @version 0.1
- * @author Damion Young
- * 
- */
+/** Development notes
+
+...Thinking maybe that we look for ENV.COURSE_ID and, if there, we know that we're in a course and so can just wrap div#content and place menu on right of that...
+BUT maybe hide on index pages for modules, announcements, discssions, etc where we already have a RH bar...
+
+All Canvas web pages seem to contain:
+
+div#content
+div#content-wrapper
+
+Inside these: (child of div#content but maybe with other siblings (in brackets is ENV variable that uniquely tells us we are in the page type) foolowed by how e might get identifier for this page to match up with/modules api feed)
+
+Home: 			div#course_home_content											n/a
+Discussions: 	div.discussion-collections 			(ENV.openTopics)			n/a
+Discussion: 	div#discussion_container 			(ENV.DISCUSSION) 			ENV.DISCUSSION.TOPIC.ID/Module | Item | content_id
+Announcements:	div.announcements-v2__wrapper 		(ENV.ANNOUNCEMENTS_LOCKED)	n/a
+Announcement:	div#discussion_container 			(ENV.DISCUSSION)			Don't seem to be able to add announcement to module so won't appear in modules api
+Assignments: 	div data-view="assignmentGroups"	(ENV.HAS_ASSIGNMENTS)		NOTE: Assignments doesn't expose ENV.COURSE_ID so menu won't be shown!
+Assignment:		div#assignment_show					(ENV.ASSIGNMENT_ID)			ENV.ASSIGNMENT_ID/Module | Item | content_id
+Grades:																			NOTE: Grades doesn't expose ENV.COURSE_ID so menu won't be shown!
+People:																			NOTE: People doesn't expose ENV.COURSE_ID so menu won't be shown!
+Pages: 																			Actually quite useful to have it in Pages!
+Files: 			div.ef-main							(ENV.FILES_CONTEXTS)		n/a
+Syllabus:																		NOTE: Syllabus doesn't expose ENV.COURSE_ID so menu won't be shown!
+Quizzes:																		Actually quite useful to have it in Quizzes!
+Quiz:			div#quiz_show						(ENV.QUIZ)					ENV.QUIZ.id/Module | Item | content_id
+Modules:		div#context_modules					(ENV.MODULE_FILE_DETAILS)	n/a
+Conferences: 																	NOTE: Conferences doesn't expose ENV.COURSE_ID so menu won't be shown!
+Collaborations: 																NOTE: Collaborations doesn't expose ENV.COURSE_ID so menu won't be shown!
+Chat: 																			NOTE: Chat doesn't expose ENV.COURSE_ID (although does have ENV.course_id) so menu won't be shown!
+Attendance: 																	NOTE: Attendance doesn't expose ENV.COURSE_ID so menu won't be shown!
+Settings: 		div#course_details_tabs				(ENV.SETTINGS)				n/a
+
+Things which can be added to a module and may therefore appear in Modules api feed:
+
+Assignment
+Quiz
+File
+Content Page
+Discussion
+Text header
+External URL
+External tool
+*/
+
 
 /* Global variables */
 var courseId = getCourseId(); //which course are we in
-var moduleItemId = getParameterByName('module_item_id');  //used to id active page where data aren't in ENV
+var moduleItemId = getParameterByName('module_item_id');  //where courseId doesn't exist 
 
+var divCourseHomeContent = document.getElementById('course_home_content');
 var divPageTitle = document.querySelectorAll('.page-title')[0];
 
-/* list of pages to exclude from menu-showing */
-/*Note that Conferences, Collaborations, Chat and Attendance pages don't expose ENV.COURSE_ID (although does have ENV.course_id) so menu won't be shown anyway...so no need to exclude */
 var dontShowMenuOnTheseElementIds=new Array(
-    'course_home_content', //Home page
-    'context_modules', //Modules page
-    'course_details_tabs' //Settings page
+    'course_home_content', 
+    'context_modules',
+    'course_details_tabs'
     );
 var dontShowMenuOnTheseElementClasses=new Array(
-    'discussion-collections', //Discussions page
-    'announcements-v2__wrapper', //Announcements page
-    'ef-main', //Files page
-    'edit-content'  //editing a wiki page
+    'discussion-collections', 
+    'announcements-v2__wrapper',
+    'ef-main'
     );
-
-/* list of pages where we want menu in right-side-wrapper */
 var putMenuInRightSideOnTheseElementIds=new Array(
-    'discussion_container', //showing a discussion
-    'quiz_show', //showing a quiz
-    'assignment_show' //showing an asignment
+    'discussion_container', 
+    'quiz_show',
+    'assignment_show'
     );
 
 var divContent = document.getElementById('content');
 
+var delimiter = '.' //The character used to separate your module name and module description
+
+//From: https://stackoverflow.com/questions/4793604/how-to-insert-an-element-after-another-element-in-javascript-without-using-a-lib
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
 /* Trying plain JS so that it works in the app as well */
 function domReady () {
+	//populate progress bars
+	showProgressBars();
 	if(divContent && courseId && courseId==2777 && elementsWithTheseIdsDontExist(dontShowMenuOnTheseElementIds) && elementsWithTheseClassesDontExist(dontShowMenuOnTheseElementClasses)){
+		//console.log("I'm in a course");
 		getSelfThenModulesForPage();
-	} 
+	} else if(divCourseHomeContent){
+		//we're in a home page
+		rewriteModuleLinks();
+	}
+}
+
+//Function to work out when the DOM is ready: https://stackoverflow.com/questions/1795089/how-can-i-detect-dom-ready-and-add-a-class-without-jquery/1795167#1795167
+// Mozilla, Opera, Webkit 
+if ( document.addEventListener ) {
+	document.addEventListener( "DOMContentLoaded", function(){
+		document.removeEventListener( "DOMContentLoaded", arguments.callee, false);
+		domReady();
+	}, false );
+// If IE event model is used
+} else if ( document.attachEvent ) {
+	// ensure firing before onload
+	document.attachEvent("onreadystatechange", function(){
+		if ( document.readyState === "complete" ) {
+			document.detachEvent( "onreadystatechange", arguments.callee );
+			domReady();
+		}
+	});
 }
 
 /**
@@ -74,6 +138,8 @@ function getSelfThenModulesForPage() {
  */
 function elementsWithTheseIdsDontExist(ids) {
     for(var i = 0; i < ids.length; i++) {
+        //console.log(ids[i]);
+        //console.log(document.getElementById(ids[i]));
         if(document.getElementById(ids[i])!==null){
             return false; //it does exist
         }
@@ -88,9 +154,9 @@ function elementsWithTheseIdsDontExist(ids) {
  */
 function elementsWithTheseClassesDontExist(classes) {
    for(var i = 0; i < classes.length; i++) {
-       console.log(classes[i]);
-       console.log(document.querySelectorAll('.'+classes[i]));
-       if(document.querySelectorAll('.'+classes[i]).length!==0){
+        //console.log(classes[i]);
+        //console.log(document.querySelectorAll(classes[i])[0]);
+        if(document.querySelectorAll(classes[i]).length!==0){
             return false; //it does exist
         }
     }
@@ -175,21 +241,7 @@ function getModulesForPage(courseId, userId) {
 				//create module div
 				var newModule = document.createElement('div');
 				newModule.className = 'ou-module-wrapper';
-                //div for module name and arrow
-                var newModuleName = document.createElement('div');
-                newModuleName.className = 'ou-menu-module-title';
-                newModuleName.setAttribute('title', moduleName);
-                newModuleName.setAttribute('data-module-id', moduleId);
-                //i for arrow
-                var newModuleArrow = document.createElement('i');
-                newModuleArrow.classList.add('icon-mini-arrow-right');
-                newModuleArrow.classList.add('ou-menu-module-arrow');
-                newModuleArrow.setAttribute('id', 'ouModuleTitleArrow_' + moduleId);
-                newModuleArrow.setAttribute('data-module-id', moduleId);
-                newModuleName.appendChild(newModuleArrow);
-                newModuleName.appendChild(document.createTextNode(moduleName));
-                newModule.appendChild(newModuleName); //add module name to module wrapper
-				//newModule.innerHTML = '<div class="ou-menu-module-title" title="' + moduleName + '" data-module-id="' + moduleId + '"><i id="ouModuleTitleArrow_' + moduleId + '" class="icon-mini-arrow-right ou-menu-module-arrow" data-module-id="' + moduleId + '"></i> ' + moduleName + '</div>';
+				newModule.innerHTML = '<div class="ou-menu-module-title" title="' + moduleName + '" data-module-id="' + moduleId + '"><i id="ouModuleTitleArrow_' + moduleId + '" class="icon-mini-arrow-right ou-menu-module-arrow" data-module-id="' + moduleId + '"></i> ' + moduleName + '</div>';
 				var moduleItemsWrapper = document.createElement('div');
 				moduleItemsWrapper.className = 'toggle-content';
 				moduleItemsWrapper.id = 'ouModuleItemsWrappper_'+moduleId;
@@ -227,13 +279,15 @@ function getModulesForPage(courseId, userId) {
 					moduleItemsWrapper.appendChild(newItem); //add item to module
 					
 				    /* Check if this is the live item and leave menu open if it is */
-                    var activateIt = false;
-					if(ENV.WIKI_PAGE){
+					console.log(moduleItemId);
+                    console.log(item.id);
+                    if(ENV.WIKI_PAGE){
 						//we're in a wiki page
 						if(item.page_url){
 							//we're processing a wiki page
 							if(ENV.WIKI_PAGE.url==item.page_url) {
-								activateIt = true;
+								moduleItemsWrapper.classList.add('is-visible');
+								newItem.classList.add('ou-menu-item-active');
 							}
 						}
 					} else if (ENV.DISCUSSION && ENV.DISCUSSION.TOPIC) {
@@ -241,7 +295,8 @@ function getModulesForPage(courseId, userId) {
                         if(item.content_id){
 							//we're processing a discussion page
 							if(ENV.DISCUSSION.TOPIC.ID==item.content_id) {
-								activateIt = true;
+								moduleItemsWrapper.classList.add('is-visible');
+								newItem.classList.add('ou-menu-item-active');
 							}
 						}
                     } else if (ENV.QUIZ) {
@@ -249,7 +304,8 @@ function getModulesForPage(courseId, userId) {
                         if(item.content_id){
 							//we're processing a quiz/survey page
 							if(ENV.QUIZ.id==item.content_id) {
-								activateIt = true;
+								moduleItemsWrapper.classList.add('is-visible');
+								newItem.classList.add('ou-menu-item-active');
 							}
 						}
                     } else if (ENV.ASSIGNMENT_ID) {
@@ -257,24 +313,20 @@ function getModulesForPage(courseId, userId) {
                         if(item.content_id){
 							//we're processing an assignment
 							if(ENV.ASSIGNMENT_ID==item.content_id) {
-								activateIt = true;
+								moduleItemsWrapper.classList.add('is-visible');
+								newItem.classList.add('ou-menu-item-active');
 							}
 						}
                     } else if (moduleItemId && parseInt(moduleItemId)===parseInt(item.id)) {
                         //we're in something else but inside a module
-                        activateIt = true;
-					}
-                    if(activateIt){
-                        /* open relevenat module and highlight active item */
                         moduleItemsWrapper.classList.add('is-visible');
-				        newItem.classList.add('ou-menu-item-active'); 
-                        /* change module arrow from right to down */ 
-                        newModuleArrow.classList.remove('icon-mini-arrow-right')
-				        newModuleArrow.classList.add('icon-mini-arrow-down');
-                    }
+				        newItem.classList.add('ou-menu-item-active');
+					}
+					
+					
 				});
 				
-				divMenuWrapper.appendChild(newModule); //add module to menu
+				divMenuWrapper.appendChild(newModule); //add module to content
 			
 			});
 		})
@@ -284,26 +336,64 @@ function getModulesForPage(courseId, userId) {
 	);
 }
 
-/* Utility functions */
-
-//Function to work out when the DOM is ready: https://stackoverflow.com/questions/1795089/how-can-i-detect-dom-ready-and-add-a-class-without-jquery/1795167#1795167
-// Mozilla, Opera, Webkit 
-if ( document.addEventListener ) {
-	document.addEventListener( "DOMContentLoaded", function(){
-		document.removeEventListener( "DOMContentLoaded", arguments.callee, false);
-		domReady();
-	}, false );
-// If IE event model is used
-} else if ( document.attachEvent ) {
-	// ensure firing before onload
-	document.attachEvent("onreadystatechange", function(){
-		if ( document.readyState === "complete" ) {
-			document.detachEvent( "onreadystatechange", arguments.callee );
-			domReady();
-		}
+/*
+ * Function which replaces all <div id="module_x" class="ou-insert-progress-bar">y</div> with graphical progress bars
+ * x = module number
+ * y = % complete
+ */
+function showProgressBars() {
+	//get all elements with classname ou-insert-progress-bar
+	var progressBarPlaceholders = document.getElementsByClassName('ou-insert-progress-bar');
+	Array.prototype.forEach.call(progressBarPlaceholders, function(progressBarPlaceholder) {
+		var value = progressBarPlaceholder.innerHTML;
+		var className = progressBarPlaceholder.id;
+		//UC first letter
+		var viewName = className.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+			return letter.toUpperCase();
+		});
+		//replace underscore with space
+		viewName = viewName.replace(/_/g, ' ');
+		//create our new element
+		var progressBarContainer = document.createElement("div");
+		progressBarContainer.innerHTML = ''+
+				'<h4 class="ou-space-before-progress-bar">Current position in ' + viewName + ':</h4>' +
+				'<div class="ou-ProgressBar ' + className + '" style="width: 100%; height: 15px;" role="progressbar" aria-valuemax="100" aria-valuemin="0" aria-valuenow="'+ value +'">' +
+				'	<div class="ou-ProgressBarBar" style="width: '+ value +'%;" title="'+ value +'%"></div>' +
+				'</div>';
+		//insert it after the placeholder using the function insertAfter
+		insertAfter(progressBarContainer, progressBarPlaceholder);
+		//now delete the placeholder
+		progressBarPlaceholder.parentNode.removeChild(progressBarPlaceholder);
 	});
 }
 
+/*
+ * Rewriting links in the format https://oxforduniversity.instructure.com/courses/[courseId]/modules/[moduleId]/items/first
+ * which works in the app by taking users to the Modules page and opens the appropriate Module
+ * However, in the web app, we need to rewrite this to go to the appropriate anchor on the Modules page
+ */
+function rewriteModuleLinks() {
+	var moduleLinks = document.getElementsByTagName('a'), i;
+    for (i in moduleLinks) {
+        var cn = moduleLinks[i].className;
+		var matchClass = "ou-ModuleLink";
+        if(cn && cn.match(new RegExp("(^|\\s)" + matchClass + "(\\s|$)"))) {
+			moduleLinks[i].onclick = function(event) {
+				var destination = event.currentTarget.href;  //attached to the a, even if user clicked something inside the a
+				var destinationParts = destination.split('/');
+				j=0;
+				while (destinationParts[j]!="modules"){
+					j=j+1;
+				}
+				var moduleId = destinationParts[j + 1];
+				window.location = "https://oxforduniversity.instructure.com/courses/" + courseId + "/modules" + "#module_" + moduleId;
+				return false;  //prevent default
+			}
+        }
+    }
+}
+
+/* Utility functions */
 /*
  * Function which returns a promise (and error if rejected) if response status is OK
  */
